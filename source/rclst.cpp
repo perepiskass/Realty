0178dd22682c884077c8ddda505e4c4169e57bb5
perepiskass <sise07@gmail.com>
2020-06-12 11:41:09 +0300

    add serelizator

--- source/rclst.cpp
+++ source/rclst.cpp
@@ -2,6 +2,8 @@
 #include <boost/filesystem.hpp>
 #include "utility.h"
 #include <dlib/svm_threaded.h>
+#include  <map>
+
 
 std::pair<size_t,std::string> checkArg(int argc,char** argv);
 
@@ -19,7 +21,7 @@ int main(int argc, char *argv[])
     kkmeans<kernel_type> test(kc);
 
     std::vector<sample_type> samples;
-    std::vector<full_sample_type> full_samples;
+    std::vector<full_sample_type> full_samples, full_samples_2;
     std::vector<sample_type> initial_centers;
 
     full_sample_type f;
@@ -71,20 +73,78 @@ int main(int argc, char *argv[])
     trainer.set_trainer(poly_trainer, 1, 2);
 
     std::cout <<"START TRAIN multiclass_classificator...\n";
-    one_vs_one_decision_function<ovo_trainer,decision_function<poly_kernel>,decision_function<rbf_kernel>> df = trainer.train(samples, labels);
+    one_vs_one_decision_function<ovo_trainer,decision_function<poly_kernel>,decision_function<rbf_kernel> >df2, df3, df = trainer.train(samples, labels);
     std::cout << "TEST multiclass_classificator\n";
     std::cout << "predicted label: "<< df(samples[0])  << ", true label: "<< labels[0] << std::endl;
     std::cout << "predicted label: "<< df(samples[90]) << ", true label: "<< labels[90] << std::endl;
+    std::cout << "predicted label: "<< df(convert_to_sample(full_samples[0]))  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df(convert_to_sample(full_samples[90])) << ", true label: "<< labels[90] << std::endl;
+
+    // one_vs_one_decision_function<ovo_trainer,decision_function<poly_kernel>, decision_function<rbf_kernel> > df2, df3;
+
+    df2 = df;
+
+    std::cout << "TEST multiclass_classificator - 2\n";
+    std::cout << "predicted label: "<< df2(samples[0])  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df2(samples[90]) << ", true label: "<< labels[90] << std::endl;
+    std::cout << "predicted label: "<< df2(convert_to_sample(full_samples[0]))  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df2(convert_to_sample(full_samples[90])) << ", true label: "<< labels[90] << std::endl;
 
     // write classificator and data to files
     namespace fs = boost::filesystem;
     if(fs::exists(fs::path(name+".clas"))) fs::remove(fs::path(name+".clas"));
-    serialize(name + ".clas") << df;
+    serialize(name + ".clas") << df2;
     std::cout << "SERIALIZE classificator done\n";
 
-    if(fs::exists(fs::path(name+".data"))) fs::remove(fs::path(name+".clas"));
+    if(fs::exists(fs::path(name+".data"))) fs::remove(fs::path(name+".data"));
     serialize(name + ".data") << full_samples;
     std::cout << "SERIALIZE data done\n";
+
+    std::map<size_t,std::vector<full_sample_type>> type_samples , t_s;
+    deserialize(name + ".clas") >> df3;
+    std::cout << "TEST multiclass_classificator after deserialize - 1\n";
+    std::cout << "predicted label: "<< df3(samples[0])  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df3(samples[90]) << ", true label: "<< labels[90] << std::endl;
+    std::cout << "predicted label: "<< df3(convert_to_sample(full_samples[0]))  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df3(convert_to_sample(full_samples[90])) << ", true label: "<< labels[90] << std::endl;
+
+    deserialize(name + ".data") >> full_samples_2;
+    std::cout << "TEST multiclass_classificator after deserialize - 2\n";
+    std::cout << "predicted label: "<< df3(samples[0])  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df3(samples[90]) << ", true label: "<< labels[90] << std::endl;
+    std::cout << "predicted label: "<< df3(convert_to_sample(full_samples_2[0]))  << ", true label: "<< labels[0] << std::endl;
+    std::cout << "predicted label: "<< df3(convert_to_sample(full_samples_2[90])) << ", true label: "<< labels[90] << std::endl;
+    
+
+    for(const auto& s : full_samples)
+    {
+      type_samples[df3(convert_to_sample(s))].push_back(s);
+    }
+    if(fs::exists(fs::path(name+".map"))) fs::remove(fs::path(name+".map"));
+    serialize(name + ".map") << type_samples;
+    std::cout << "SERIALIZE map-data done\n";
+
+    std::cout << "BEFOR DESERIALIZE: \n";
+    for(auto& i : type_samples[3])
+    {
+      for(auto& z : i)
+      {
+        std::cout << z << ' ';
+      }
+      std::cout << std::endl;
+    }
+
+
+    deserialize(name + ".map") >> t_s;
+    std::cout << "DESERIALIZE DONE\n";
+    for(auto& i : t_s[3])
+    {
+      for(auto& z : i)
+      {
+        std::cout << z << ' ';
+      }
+      std::cout << std::endl;
+    }
   }
   catch(const std::exception& e)
   {
